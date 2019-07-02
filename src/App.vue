2ab2aad35e90daa69c99ff9390e1d86b8f90a2f9
王振 <zhen.wang@lanmaoly.com>
2019-07-02 15:48:06 +0800

    代码调整

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,27 @@
     <div class="flex1 div-content">
       <router-view></router-view>
     </div>
+    <div class="div-nav">
+      <el-menu default-active="/home" class="el-menu-vertical-demo" @select="handleSelect">
+        <el-menu-item index="/personal">
+          <i class="iconshouye iconfont"></i>
+          <span slot="title">系统首页</span>
+        </el-menu-item>
+        <el-menu-item index="/accounting">
+          <i class="iconshouye iconfont"></i>
+          <span slot="title">薪资核算</span>
+        </el-menu-item>
+        <el-menu-item index="/personal">
+          <i class="iconfont iconshenbao"></i>
+          <span slot="title">个税申报</span>
+        </el-menu-item>
+        <el-menu-item index="/personal">
+          <i class="iconfont iconicon-test"></i>
+          <span slot="title">工资条</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+
   </div>
 </template>
 <script>
