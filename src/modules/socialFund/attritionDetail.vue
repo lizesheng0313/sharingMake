93f57be9151310bccbb536df7bdd719bdddbee99
李颖 <ying.li@lanmaoly.com>
2020-02-24 15:39:32 +0800

    继续参保修改

--- src/modules/socialFund/attritionDetail.vue
+++ src/modules/socialFund/attritionDetail.vue
@@ -37,7 +37,7 @@
                   <span class="fun-right">
                       <el-button type="primary" @click="editInsured">编辑</el-button>
                       <el-button type="primary" @click="decreaceStaff" v-if="floatEmployeeBaseInfoVo.insuredStatus === 'INSURED_ING'" v-show="privilegeVoList.includes('salary.social.floatEmployee.decrease')">减员</el-button>
-                      <el-button type="primary" @click="increaceStaff" v-if="floatEmployeeBaseInfoVo.insuredStatus === 'INSURED_STOP'" >继续参保</el-button>
+                      <el-button type="primary" @click="increaceStaff" v-else >继续参保</el-button>
 
                 </span>
               </div>
