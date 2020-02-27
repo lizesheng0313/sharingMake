b7445f3eb65c8eb2dc0501de83023b7fb17189f8
李颖 <ying.li@lanmaoly.com>
2020-02-27 19:39:48 +0800

    缴款二个提示删除

--- src/modules/taxPaid/components/paidFeedback.vue
+++ src/modules/taxPaid/components/paidFeedback.vue
@@ -85,7 +85,7 @@ export default {
             this.$refs.authorizeTip.show()
           }
         }else{
-          this.$message.warning(res.message)
+          // this.$message.warning(res.message)
         }
       })
     },
